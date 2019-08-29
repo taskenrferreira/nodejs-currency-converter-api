4b7242cc91bf66c1627dda2bf5ec2a9390d3c999
Raphael <rferreira@tasken.com>
2019-08-29 19:51:32 -0300

    Incluído controller

--- src/server.js
+++ src/server.js
@@ -2,27 +2,32 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const axios = require('axios')
 var currency = require('./currency')
+var list = require('./models/database.json')
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-    var result = 0
+// app.get('/', (req, res) => {
+//     var result = 0
 
-    currency = req.query
+//     currency = req.query
 
-    const url = `https://api.exchangeratesapi.io/latest?base=${currency.from}`
+//     const url = `https://api.exchangeratesapi.io/latest?base=${currency.from}`
 
-    axios.get(url).then(response => {        
-        rate = response.data.rates[currency.to]
+//     axios.get(url).then(response => {        
+//         rate = response.data.rates[currency.to]
 
-        result = currency.amount * rate        
-        console.log(result)
+//         result = currency.amount * rate        
+//         console.log(result)
 
-        return res.send({ ...currency, result })
-    })    
+//         return res.send({ ...currency, result })
+//     })    
+// })
+
+app.get('/currency',(req, res) => {
+    res.send(list)
 })
 
 app.listen(3000)
