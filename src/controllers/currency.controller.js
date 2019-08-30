0ca0dfc9616070700cdb7b13b70ab42400e412c7
Raphael <rferreira@tasken.com>
2019-08-30 20:11:42 -0300

    Add routes and controllers

--- src/controllers/currency.controller.js
+++ src/controllers/currency.controller.js
@@ -1,30 +1,11 @@
-const express = require('express')
-const bodyParser = require('body-parser')
 var fs = require('fs');
 
-const axios = require('axios')
-
-const app = express()
-
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-
-app.use(function(req, res, next){
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "content-type");
-    res.setHeader("Content-Type", "application/json");
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    next();
-});
-
-app.get('/currency', function(req, res){
+exports.get = (req, res, next) => {
     fs.readFile('./database.json', 'utf8', function(err, data){
         if (err) {
             var response = {status: 'falha', resultado: err};
-            res.json(response);
+            res.send(response);
         } else {
-            console.log(data)
             var obj = JSON.parse(data);
             var result = {status: 'falha', resultado: 'Nenhuma moeda foi encontrada' };
 
@@ -43,16 +24,15 @@ app.get('/currency', function(req, res){
             }
 
             var response = result;
-            res.json(response);
+            res.send(response);
         }
     });
-});
-
-app.get('/currency/:code', function(req, res){
+};
+exports.getByCode = (req, res, next) => {
     fs.readFile('./database.json', 'utf8', function(err, data){
         if (err) {
             var response = {status: 'falha', resultado: err};
-            res.json(response);
+            res.send(response);
         } else {            
             var obj = JSON.parse(data);
             var result = {status: 'falha', resultado: 'Nenhuma moeda foi encontrada' } ;
@@ -70,16 +50,15 @@ app.get('/currency/:code', function(req, res){
             });
 
             var response = result;
-            res.json(response);
+            res.send(response);
         }
     });
-});
-
-app.post('/currency', function(req, res){
+};
+exports.post = (req, res, next) => {
     fs.readFile('./database.json', 'utf8', function(err, data){
         if (err) {
             var response = {status: 'falha', resultado: err};
-            res.json(response);
+            res.send(response);
         } else {            
             var obj = JSON.parse(data);            
 
@@ -89,25 +68,24 @@ app.post('/currency', function(req, res){
                 fs.writeFile('./database.json', JSON.stringify(obj), function(err) {
                     if (err) {
                     var response = {status: 'falha', resultado: err};
-                    res.json(response);
+                    res.send(response);
                     } else {
                     var response = {status: 'sucesso', resultado: 'Registro incluído com sucesso'};
-                    res.json(response);
+                    res.send(response);
                     }
                 });
             } else {
                 var response = {status: 'falha', resultado: 'Nenhum registro a ser incluído'};
-                res.json(response);
+                res.send(response);
             }            
         }
     });
-});
-
-app.delete('/currency/:code', function(req, res){
+};
+exports.delete = (req, res, next) => {
     fs.readFile('./database.json', 'utf8', function(err, data){
         if (err) {
             var response = {status: 'falha', resultado: err};
-            res.json(response);
+            res.send(response);
         } else {
             var obj = JSON.parse(data);
             var codeDeleted = req.params.code.toUpperCase();
@@ -121,14 +99,12 @@ app.delete('/currency/:code', function(req, res){
             fs.writeFile('./database.json', JSON.stringify(obj), function(err) {
                 if (err) {
                     var response = {status: 'falha', resultado: err};
-                    res.json(response);
+                    res.send(response);
                 } else {
                     var response = {status: 'sucesso', resultado: `Registro ${codeDeleted} excluído com sucesso`};
-                    res.json(response);
+                    res.send(response);
                 }
             });
         }
     });
-});
-
-app.listen(3000)
+};
